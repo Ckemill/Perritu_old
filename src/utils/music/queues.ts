5ce33edd2a1ab3ebab3598e9392e8b6adb27bd03
Ckemill <Ckemill@gmail.com>
2023-04-11 13:32:09 -0400

    added getQueue function

--- src/utils/music/queues.ts
+++ src/utils/music/queues.ts
@@ -1,3 +1,14 @@
-import { Queues } from "../../types";
+import { Queue, Queues } from "../../types";
+import { Snowflake } from "discord.js";
 
 export const queues: Queues = {};
+
+export function getQueue(guildId: Snowflake): Queue | null {
+  const queue = queues[guildId];
+
+  if (!queue) {
+    return null;
+  }
+
+  return queue;
+}

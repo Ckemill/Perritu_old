5928b129f26dc43397a815e25aaf9862baf4f8a1
Ckemill <Ckemill@gmail.com>
2023-04-28 01:42:42 -0400

    changed ytdl quality to low and more buffer

--- src/utils/music/youtube/url.ts
+++ src/utils/music/youtube/url.ts
@@ -4,8 +4,8 @@ import ytdl from "ytdl-core";
 export async function YTurl(url: string): Promise<Readable> {
   const stream = ytdl(url, {
     filter: "audioonly",
-    quality: "highestaudio",
-    highWaterMark: 1 << 25,
+    quality: "lowestaudio",
+    highWaterMark: 50,
   });
 
   return stream;

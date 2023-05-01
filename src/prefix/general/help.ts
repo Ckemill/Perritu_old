be7481d252ccec1e27b441340b2d39a188ec109c
Ckemill <Ckemill@gmail.com>
2023-05-01 11:38:58 -0400

    corrected help prefix command

--- src/prefix/general/help.ts
+++ src/prefix/general/help.ts
@@ -1,7 +1,10 @@
 import { prefix } from "../../utils";
+import { getCategoryRoot } from "../../pages/help";
 
-export default prefix("ping", ({ args, message }) => {
+export default prefix("help", ({ args, message }) => {
   if (args.length == 0) return message.reply("Pong");
 
-  return message.reply(args);
+  return message.reply(
+    `Please use **/help** or go to https://perritu.com for more info.`
+  );
 });
